cf1eb05ed7c6b9b9b3926f05922220b9d7c1ec93
nirvanush <nirvanushergo@gmail.com>
2022-08-01 15:12:38 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  }
+  swcMinify: true,
 }
+
+module.exports = nextConfig
